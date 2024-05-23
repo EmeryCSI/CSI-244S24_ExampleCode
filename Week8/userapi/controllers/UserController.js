391e45b578270135276ddde880e87176ada1fc71
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2024-05-23 15:51:37 -0700

    Updated

--- Week8/userapi/controllers/UserController.js
+++ Week8/userapi/controllers/UserController.js
@@ -6,6 +6,7 @@ const User = require("../models/User");
 
 //lets make a function that returns all users
 //the will run in response to a get request.
+//GET /User
 const getAllUsers = async (req, res) => {
   try {
     //get all users from the model
@@ -15,7 +16,20 @@ const getAllUsers = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+//lets make an endpoint that takes in an id and returns a user
+//this will run in response to a get request
+const getUser = async (req, res) => {
+  try {
+    //find a user by id
+    console.log(req.params.id);
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 //this is going to be hit with a post request
+//Post /User
 const createUser = async (req, res) => {
   //create a new user from the body of the request
   const user = new User({
@@ -33,5 +47,35 @@ const createUser = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+//Put user/ID
+//update a user. this is goind to be hit with a put request
+//remember that a put request gets data from the body AND the url
+const updateUser = async (req, res) => {
+  const userInfo = req.body;
+  try {
+    //find a user by id and update it
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      userInfo,
+      //this option returns the updated user
+      { new: true }
+    );
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+//Delete user/ID
+//delete a user. this is going to be hit with a delete request
+const deleteUser = async (req, res) => {
+  try {
+    //find a user by id and delete it
+    const user = await User.findByIdAndDelete(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 //Export these functiona to be used in another module
-module.exports = { getAllUsers, createUser };
+module.exports = { getAllUsers, createUser, getUser, updateUser, deleteUser };
--- Week8/userapi/routes/UserRoutes.js
+++ Week8/userapi/routes/UserRoutes.js
@@ -5,8 +5,11 @@ const router = express.Router();
 const userController = require("../controllers/UserController");
 
 //now we define our routes
+router.get("/:id", userController.getUser);
 router.get("/", userController.getAllUsers);
 router.post("/", userController.createUser);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 //We export the router
 module.exports = router;
